109fa81319d7e74099c1d67333cebd02277e57dc
Jacob Hokanson <hokansonjm@masters.edu>
2019-04-02 13:10:52 -0700

    Beginnings of a pygame window and event loop

--- tufftotest.py
+++ tufftotest.py
@@ -1,8 +1,17 @@
-import pygame
+import pygame, sys
+from pygame.locals import *
 
-class someGame:
-    A_CONSTANT = 1
+pygame.init()
+DISPLAY = pygame.display.set_mode((800, 600))
+DISPLAY.fill((255, 255, 255))
+pygame.display.set_caption("Pyon's Blaze")
 
-    def __init__(self):
-        self.num = 1
-        self.string = 'pie'
+
+while True:
+    
+    for event in pygame.event.get():  # Begin main event loop (listen, update, draw)
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
+    pygame.time.Clock().tick(30)
