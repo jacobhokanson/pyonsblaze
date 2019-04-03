673be8ddab2336cc7237e169c4bbfd7b6e8a3a4e
mageecd <mageecd@masters.edu>
2019-04-03 15:45:38 -0700

    Added text to cat moving in a square

--- pygameTest1Caleb.py
+++ pygameTest1Caleb.py
@@ -10,6 +10,15 @@ fpsClock = pygame.time.Clock()
 DISPLAYSURF = pygame.display.set_mode((400, 300), 0, 32)
 
 WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 128)
+
+fontObj = pygame.font.Font('freesansbold.ttf', 32)
+textSurfaceObj = fontObj.render("Hello World!", True, GREEN, BLUE)
+textRectObj = textSurfaceObj.get_rect()
+textRectObj.center = (200, 150)
+
+
 catImg = pygame.image.load('cat.png')
 catx = 10
 caty = 10
@@ -17,6 +26,7 @@ direction = 'right'
 
 while True: # main game loop
     DISPLAYSURF.fill(WHITE)
+    DISPLAYSURF.blit(textSurfaceObj, textRectObj)
     if direction == 'right':
         catx += 5
         if catx == 280:
